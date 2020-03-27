11ce0d7471159783bcc4c001e0ffb01ef9bd73a3
Ugo Loobuyck <ugo813@hotmail.fr>
2020-03-27 13:01:10 +0100

    compute_w ready to test

--- kendall_w/kendall_w.py
+++ kendall_w/kendall_w.py
@@ -1,29 +1,53 @@
-def kendall_w(arr):
+import pandas as pd
+import warnings
+
+def compute_w(arr):
     """ Computes kendall's W from a list of rating lists
-    Arguments
+    Arguments.
+    0 indicates no agreement and 1 indicates unanimous agreement.
     ---------
 
     arr : list
-        List of list with shape (n_items * n_annotators)
+        List of lists with shape (n_items * n_annotators)
 
     Return
     ---------
 
     W : float
-        Kendall's W [0:1] 
+        Kendall's W [0:1]
+    
+    Example
+    ---------
+
+    annotations = [
+        [1, 1, 1, 2], # item 1
+        [2, 2, 2, 3], # item 2
+        [3, 3, 3, 1], # item 3
+    ]
+    # Annotator #4 disagrees with the other annotators
+    # Annotators #1, #2, #3 agree
+
+    W = kendall_w(annotations)
+    # output: 0.4375
     """
 
+    assert isinstance(arr, list), "You must pass a python list, {} found".format(type(arr))
+    assert isinstance(arr[0], list), "You must pass a list of python list, {} found".format(type(arr[0]))
+
+    # Number of annotators
     m = len(arr[0])
+    if m <= 2:
+        warnings.warn("Kendall's W is adapted to the agreement measurement between more than two annotators. See Kendall's tau for more information")
+    
+    # Number of items
     n = len(arr)
+    assert n > 1, "Kendall's W is irrevelent for only one item, try adding more sublists to argument 'arr'."
 
+    # Compute sum of each item ranks
     sums = [sum(x) for x in arr]
-    print('sums', sums)
-    Rbar = sum(sums) / len(arr)
-    print('Rbar', Rbar)
-    S = sum([(sums[x] - Rbar) ** 2 for x in range(len(sums))])
-    print('S', S)
+    
+    Rbar = sum(sums) / n
+    S = sum([(sums[x] - Rbar) ** 2 for x in range(n)])
     W = (12 * S) / (m ** 2 * (n ** 3 - n))
-    print('W', W)
 
-# alte = [[2,2,2,2], [3,3,3,3], [4,4,4,4]]
-# kendall_w(alte)
+    return W
