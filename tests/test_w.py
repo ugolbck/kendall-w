7cfb504a901fe74404da58e45e769ff114cada82
Ugo Loobuyck <ugo813@hotmail.fr>
2020-03-29 10:32:59 +0200

    basic test definition

--- tests/test_w.py
+++ tests/test_w.py
@@ -4,14 +4,26 @@ from kendall_w.kendall_w import compute_w
 class TestComputeW(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.perfect_agg = [[1,1,1], [2,2,2], [3,3,3]]
+        self.perfect_disagg = [[1,2,3], [2,3,1], [3,1,2]]
     
     def tearDown(self):
         pass
 
-    def test_(self):
-        self.assertEqual(compute_w([[1,1,1], [2,2,2], [3,3,3]]), 1)
+    def test_result(self):
+        self.assertEqual(compute_w(self.perfect_agg), 1)
 
+    def test_input_types(self):
+        pass
+
+    def test_diff_list_elem(self):
+        pass
+
+    def test_diff_sublist_elem(self):
+        pass
+
+    def test_ranking(self):
+        pass
 
 if __name__ == '__main__':
     unittest.main()
