aa3e7eed9eb5debb1494cc4142b3a4c514f63fcb
Ugo Loobuyck <ugo813@hotmail.fr>
2020-03-29 10:25:12 +0200

    first unit test and assert addition

--- kendall_w/kendall_w.py
+++ kendall_w/kendall_w.py
@@ -1,3 +1,16 @@
+# TODO:
+# - Perform series of tests to make compute_w robust
+# - Add possibility to compute from pd.DataFrame:
+#       - Separate function that allows a DataFrame
+#       - Force DataFrame in args[0] of compute_w
+#       - Check type of args[0] of compute_w and convert DataFrame to matrix
+#       - Check type of args[0] of compute_w and write additionnal code adapted to pandas.DataFrame
+# - Check that all elements of args[0] are lists and all elements of sublists are integers:
+#       -> Exception
+# - Check that items are RANKED for each annotator:
+#       -> Exception or Warning ?
+
+
 import warnings
 
 def compute_w(arr):
@@ -30,14 +43,15 @@ def compute_w(arr):
     W = kendall_w(annotations)
     # output: 0.4375
     """
-
+    
     assert isinstance(arr, list), "You must pass a python list, {} found".format(type(arr))
-    assert isinstance(arr[0], list), "You must pass a list of python lists, {} found".format(type(arr[0]))
+    assert all(isinstance(x, list) for x in arr), "You must pass a list of python lists as input." # To test
+    assert all(isinstance(x[y], int) for x in arr for y in range(len(x))), "You must pass a list of integer lists." # To test
 
     # Number of annotators
     m = len(arr[0])
     if m <= 2:
-        warnings.warn("Kendall's W is adapted to the agreement measurement between more than two annotators. See Kendall's tau for more information.")
+        warnings.warn("Kendall's W is adapted to measure agreement between more than two annotators. The results might not be reliable in this case.")
     
     # Number of items
     n = len(arr)
@@ -49,7 +63,7 @@ def compute_w(arr):
     Rbar = sum(sums) / n
     # Sum of squared deviations from the mean
     S = sum([(sums[x] - Rbar) ** 2 for x in range(n)])
-    
+
     W = (12 * S) / (m ** 2 * (n ** 3 - n))
 
     return W
--- tests/test_w.py
+++ tests/test_w.py
@@ -1,5 +1,17 @@
 import unittest
+from kendall_w.kendall_w import compute_w
 
-class TestStringMethods(unittest.TestCase):
+class TestComputeW(unittest.TestCase):
 
-    pass
+    def setUp(self):
+        pass
+    
+    def tearDown(self):
+        pass
+
+    def test_(self):
+        self.assertEqual(compute_w([[1,1,1], [2,2,2], [3,3,3]]), 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
