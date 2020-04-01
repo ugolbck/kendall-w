5838ff5a978ac9325f48c931209ab8df19d8182f
Ugo Loobuyck <ugo813@hotmail.fr>
2020-04-01 11:24:39 +0200

    init modification of imports

--- kendall_w/__init__.py
+++ kendall_w/__init__.py
@@ -1,1 +1,7 @@
+from .kendall_w import kendall_w
+# Next version: usage goes from 
+# `from kendall_w.kendall_w import compute_w` to
+# `from kendall_w import compute-w`
+
 __all__ = ['kendall_w']
+__version__ = (0, 0, 1)
