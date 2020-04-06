c865ab73a61cdd941a1e1eb46fe2598798a53313
Ugo Loobuyck <ugo813@hotmail.fr>
2020-04-06 12:12:56 +0200

    modif test_w 7

--- kendall_w/__init__.py
+++ kendall_w/__init__.py
@@ -1,8 +1,3 @@
-# from .kendall_w import compute_w
+from .kendall_w import compute_w
 
-# Next version: usage goes from 
-# `from kendall_w.kendall_w import compute_w` to
-# `from kendall_w import compute-w`
-
-__all__ = ['kendall_w']
 __version__ = (0, 0, 3)
--- tests/test_w.py
+++ tests/test_w.py
@@ -1,5 +1,5 @@
 import unittest
-import kendall_w
+from kendall_w import compute_w
 
 class TestComputeW(unittest.TestCase):
 
